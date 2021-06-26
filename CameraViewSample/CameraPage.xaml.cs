49e6e760f2a3c7958a42963ed8de7388e6a46879
Gustavo Oliveira <gu.ol@hotmail.com>
2021-06-26 11:39:16 +1000

    Created CameraView Sample

---
+++ CameraViewSample/CameraPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Xamarin.CommunityToolkit.UI.Views;
+using Xamarin.Forms;
+
+namespace CameraViewSample
+{
+    public partial class CameraPage : ContentPage
+    {
+		int counter = 0;
+		public CameraPage()
+		{
+			InitializeComponent();
+		}
+
+		public void CameraView_OnAvailable(object sender, bool e)
+		{
+			shutterButton.IsEnabled = e;
+		}
+
+		public void CameraView_MediaCaptured(object sender, MediaCapturedEventArgs e)
+		{
+			shutterButton.Text = $"{++counter}";
+			previewPicture.Source = e.Image;
+			previewPicture.Rotation = e.Rotation;
+		}
+	}
+}
---
+++ CameraViewSample/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CameraViewSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Task.Run(async ()=> await LongProcessTask());
+		}
+
+        private async Task LongProcessTask()
+        {
+            await Task.Delay(TimeSpan.FromMinutes(1));
+        }
+
+        public async void CameraButton_Clicked(object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new CameraPage());
+        }
+    }
+}
